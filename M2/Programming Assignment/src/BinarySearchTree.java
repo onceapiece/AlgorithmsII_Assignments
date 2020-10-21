f3c0fa7285b362359b75e3a0492451027e3cc191
Colin <colin.mcallister89@yahoo.com>
2020-10-21 10:13:55 -0700

    Fixed error in code causing growth to look like N

--- M2/Programming Assignment/src/BinarySearchTree.java
+++ M2/Programming Assignment/src/BinarySearchTree.java
@@ -62,7 +62,6 @@ public class BinarySearchTree {
         pwOb.flush();
         StringBuilder dataSB = new StringBuilder();
 
-        BinarySearchTree T = new BinarySearchTree();
         Random rand = new Random();
 
         for (int n = 500; n <= 20000 ; n = n + 500) {
@@ -72,6 +71,7 @@ public class BinarySearchTree {
             double heightOverN = 0;
             double logN = Math.log(n) / Math.log(2);
             for (int j = 0; j < 10; j++) { // take 10 measurements
+                BinarySearchTree T = new BinarySearchTree();
                 for (int i = 0; i < n; i++) {
                     Node z = new Node(rand.nextInt(n));
                     treeInsert(T, z);
