21301f879920030defca763765a4f8562285ac7e
Colin <colin.mcallister89@yahoo.com>
2020-11-14 08:52:43 -0800

    silly changes

--- M5/Exercise 2 Java Files/src/MemoizedCutRod.java
+++ M5/Exercise 2 Java Files/src/MemoizedCutRod.java
@@ -3,32 +3,33 @@
 public class MemoizedCutRod {
 
 
-   public int MemoizedCutRod(int[] p, int n) {
-      int[] r = new int[n+1];
-      for (int i = 0; i <= n ; i++) {
-         r[i] = -1;
-      }
-      return MemoizedCutRodAux(p, n, r);
-   }
-
-   public int MemoizedCutRodAux(int[] p, int n, int[] r) {
-      int q = -1;
-      if (r[n] >= 0) {
-         return r[n];
-      }
-      if (n==0) {
-         q = 0;
-      }
-      for (int i = 1; i <= n ; i++) {
-         q = Math.max(q, p[i] + MemoizedCutRodAux(p, n-i, r));
-      }
-      r[n] = q;
-      return q;
-   }
+//   public int MemoizedCutRod(int[] p, int n) {
+//      int[] r = new int[n+1];
+//      for (int i = 0; i <= n ; i++) {
+//         r[i] = -1;
+//      }
+//      return MemoizedCutRodAux(p, n, r);
+//   }
+//
+//   public int MemoizedCutRodAux(int[] p, int n, int[] r) {
+//      int q = -1;
+//      if (r[n] >= 0) {
+//         return r[n];
+//      }
+//      if (n==0) {
+//         q = 0;
+//      }
+//      for (int i = 1; i <= n ; i++) {
+//         q = Math.max(q, p[i] + MemoizedCutRodAux(p, n-i, r));
+//      }
+//      r[n] = q;
+//      return q;
+//   }
 
    public static int[][] MemoizedCutRodSolution(int[] prices, int n) {
       int[] results = new int[n+1];
       int[] size = new int[n+1];
+
       for (int i = 0; i <= n ; i++) {
          results[i] = -1;
       }
@@ -44,7 +45,7 @@ public class MemoizedCutRod {
       if (n==0) {
          q = 0;
       }
-      for (int i = 1; i <= n ; i++) {
+      for (int i = 1; i <= n; i++) {
          int b = prices[i] + MemoizedCutRodSolutionAux(prices, n-i, results, size);
          if (q < b) {
             q = b;
